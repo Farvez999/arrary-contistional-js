43ca3e4976f8fd90c7e9abbcd76e043cd58198df
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-01 00:27:46 +0600

    2 number compare

---
+++ comparison.js
@@ -0,0 +1,19 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(5 == 5);
+// console.log(5 != 5);
+// console.log(5 <= 6);
+// console.log(16 <= 6);
+// console.log(5 >= 6);
+
+var num1 = 40;
+var num2 = 50;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 == num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 != num2);
+
+// multiple condistion
