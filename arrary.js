283d680884a322babbfc276040797c57529dee06
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-07-31 22:22:52 +0600

    set index of element

---
+++ arrary.js
@@ -0,0 +1,3 @@
+var friendsAge = [11, 22, 54, 67, 105, 7];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels);
---
+++ index.js
@@ -0,0 +1,13 @@
+var numbers = [54, 65, 85, 64, 77]
+
+//get element by index
+var element = numbers[1];
+console.log(element);
+
+//set element value by index
+numbers[3] = 77;
+console.log(numbers);
+
+// find index of element
+var positionIndex = numbers.indexOf(77);
+console.log(positionIndex);
