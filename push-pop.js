a44db21c7f1efa141ae9db79c27a9a4377585717
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-01 00:14:19 +0600

    push-pop array

---
+++ push-pop.js
@@ -0,0 +1,21 @@
+var numbers = [45, 56, 34, 56, 23, 48];
+// USE PUSH to add element  an arrary as the last array
+//numbers.push(63);
+numbers.pop();
+
+// pop the 1st element in array
+numbers.shift();
+console.log(numbers);
+
+// var element = numbers.pop();
+// console.log(element);
+
+var friends = ['balam', 'kalam', 'dalam']
+friends.push('jalam');
+friends.push('palam');
+
+// push the 1st element in array
+friends.unshift('alam');
+console.log(friends);
+
+// USE POP to get last element
